c4b2da0f3a792ec6b2eebf1a3a09c1c88ee95636
= <=>
2022-08-24 17:15:42 +0700

    update post crud view

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-createApp(App).mount("#app");
+//import Bootstrap, Popper, jQuery
+import 'bootstrap/dist/css/bootstrap.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+//import router
+import router from './router'
+
+const app = createApp(App)
+
+//use vue router
+app.use(router)
+
+app.mount('#app')
